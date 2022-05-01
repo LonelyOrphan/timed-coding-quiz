06b28696ed98e2c0e51fb179b247230a6aa5eddc
Natasha Beddow <beddow90@gmail.com>
2022-05-02 00:35:13 +0100

    created renderQuestion function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,8 +14,19 @@ const removeStartupScreen = function() {
 
 // Render Question
 const renderQuestion = function() {
+
+    // Create question container section
     const section = document.createElement("section");
+    section.setAttribute("class", "question-container");
+
+    // Create question
     const h2 = document.createElement("h2");
+    h2.textContent = "What is JavaScript?"
+
+    // Create question answers
+    const ul = document.createElement("ul");
+    ul.setAttribute("class", "answers-list");
+    const div = document.createElement("div");
 }
 
 // Declare event handler function for start button click
