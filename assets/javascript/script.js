e7f2f6e250c2a857286647b78d264ada08990f23
Natasha Beddow <beddow90@gmail.com>
2022-05-01 21:26:21 +0100

    added first functions in js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,26 @@
-console.log("scriptest")
+// Target Start button
+const startButton = document.getElementById("start-btn");
+
+// Target main section
+const mainContent = document.getElementById("startup-screen");
+
+// Remove startup screen
+const removeStartupScreen = function() {
+
+}
+
+// Render Question
+const renderQuestion = function() {
+    
+}
+
+// Declare event handler function for start button click
+const handleStartButtonClick = function() {
+    console.log(startButton);
+    // 
+    removeStartupScreen();
+    renderQuestion();
+}
+
+// Add event handler to start button
+startButton.addEventListener("click", handleStartButtonClick);
