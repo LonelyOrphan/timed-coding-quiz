efac0d9b6f209f24b121af514f4ea00e4340443e
Natasha Beddow <beddow90@gmail.com>
2022-09-07 18:55:08 +0100

    added timer

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,15 @@
+let count = 31;
+
+const timerDisplay = document.getElementById("timer");
+
+// Countdown
+const countdown = () => {
+  if (count > 0) {
+    count--;
+  }
+  timerDisplay.innerHTML = count;
+};
+
 // Target Start button
 const startButton = document.getElementById("start-btn");
 
@@ -8,52 +20,49 @@ const startupScreen = document.getElementById("startup-screen");
 const mainElement = document.getElementById("main");
 
 // Target question section
-const questionElement = document.getElementById("question-section");
+const questionElement = document.getElementById("question-section-1");
 
 // Remove startup screen
-const removeStartupScreen = function() {
-    
-}
-
-// Render Question
-const renderQuestion = function() {
-
-    // Create question container section
-    const section = document.createElement("section");
-    section.setAttribute("class", "question-container");
-
-    // Create question
-    const h2 = document.createElement("h2");
-    h2.textContent = "What is JavaScript?";
-
-    // Create question answers
-    const ul = document.createElement("ul");
-    ul.setAttribute("class", "answers-list");
-    
-
-    // Loop over li items to create
-    const li1 = document.createElement("li");
-    li1.setAttribute("class", "list-item");
-    li1.textContent = "Indonesian penmanship";
-
-    const li2 = document.createElement("li");
-    li1.setAttribute("class", "list-item");
-    li2.textContent = "Indonesian penmanship";
-
-    const li3 = document.createElement("li");
-    li1.setAttribute("class", "list-item");
-    li3.textContent = "Indonesian penmanship";
-
-}
+const removeStartupScreen = function () {};
 
 // Declare event handler function for start button click
-const handleStartButtonClick = function() {
-    startupScreen.style.display = "none";
-    questionElement.style.display = "block";
+const handleStartButtonClick = function () {
+  setInterval(countdown, 1000);
+  startupScreen.style.display = "none";
+  questionElement.style.display = "block";
+  timerDisplay.style.display = "block";
 
-    removeStartupScreen();
-    renderQuestion();
-}
+  removeStartupScreen();
+  renderQuestion();
+};
 
 // Add event handler to start button
 startButton.addEventListener("click", handleStartButtonClick);
+
+// Render Question
+// const renderQuestion = function () {
+//   // Create question container section
+//   const section = document.createElement("section");
+//   section.setAttribute("class", "question-container");
+
+//   // Create question
+//   const h2 = document.createElement("h2");
+//   h2.textContent = "What is JavaScript?";
+
+//   // Create question answers
+//   const ul = document.createElement("ul");
+//   ul.setAttribute("class", "answers-list");
+
+//   // Loop over li items to create
+//   const li1 = document.createElement("li");
+//   li1.setAttribute("class", "list-item");
+//   li1.textContent = "Indonesian penmanship";
+
+//   const li2 = document.createElement("li");
+//   li1.setAttribute("class", "list-item");
+//   li2.textContent = "Indonesian penmanship";
+
+//   const li3 = document.createElement("li");
+//   li1.setAttribute("class", "list-item");
+//   li3.textContent = "Indonesian penmanship";
+// };
